ac686b3def575b8d75f56c4e8254a52a36cceea6
Azmaths <47237313+Azmaths@users.noreply.github.com>
2021-08-31 17:47:40 +0530

    added model

---
+++ DCA/src/Models/Developer/developer.ts
@@ -0,0 +1,13 @@
+export class Developer {  
+  
+    devId:number;
+    name:string;
+    email:string;
+    skillLevel:string;
+    memberSince:Date;
+    totalFeeds:number;
+    user:User;
+    feedList:number;
+    isVerified:boolean;
+    isBlocked: boolean;
+ } 
