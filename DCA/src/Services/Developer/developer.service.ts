984e9811d28f407897d0879bd5f708f1c7e40089
Azmaths <47237313+Azmaths@users.noreply.github.com>
2021-09-01 19:56:34 +0530

    added developer service class and added component class

--- DCA/src/Services/Developer/developer.service.ts
+++ DCA/src/Services/Developer/developer.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Developer } from 'src/Models/Developer/developer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeveloperService {
 
-  constructor() { }
+  baseURL = 'http://localhost:9090/developers';
+
+  constructor(private http: HttpClient) { }
+
+  getDevelopers(): Observable<any> {
+    return this.http.get(this.baseURL)
+
+  }
+
+  getDeveloper(devId: number): Observable<any> {
+    return this.http.get(`${this.baseURL}/${devId}`);
+
+  }
+
+  updateDeveloper(developer: Developer):Observable<any> {
+    return this.http.put(this.baseURL, developer)
+  }
+
+
+  deleteDeveloper(devId: number): Observable<any> {
+
+    let url = `${this.baseURL}/${devId}`
+    return this.http.delete(url, { responseType: 'text' })
+
+  }
+
+  addDeveloper(developer: Developer):Observable<any> {
+  return  this.http.post(this.baseURL, developer);
+
+  }
 }
--- DCA/src/app/Components/Developer/create-developer/create-developer/create-developer.component.ts
+++ DCA/src/app/Components/Developer/create-developer/create-developer/create-developer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Developer } from 'src/Models/Developer/developer';
+import { DeveloperService } from 'src/Services/Developer/developer.service';
 
 @Component({
   selector: 'app-create-developer',
@@ -7,12 +9,36 @@ import { Developer } from 'src/Models/Developer/developer';
   styleUrls: ['./create-developer.component.css']
 })
 export class CreateDeveloperComponent implements OnInit {
+  message: any;
 
-  constructor() { }
+  constructor(private service: DeveloperService, private router: Router) { }
 
   ngOnInit(): void {
   }
-  
 
+  msgClass: string;
 
+  createNew(data: Developer) {
+
+    this.service.addDeveloper(data).subscribe(
+      (data) => {
+        this.message = data.message;
+        this.msgClass = 'alert alert-success';
+
+      },
+
+      (fail) => {
+        this.message = fail.error.errorMessage;
+        this.msgClass = 'alert alert-danger';
+
+      }
+    )
+  }
+
+  gotoList() {
+    this.router.navigate(['dev-list'])
+  }
 }
+
+
+
