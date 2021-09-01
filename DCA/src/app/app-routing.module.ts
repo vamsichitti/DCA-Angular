0d944c85d70cd2b4d6d08a68c08aff08d6028420
vamsichitti <vamsi.chitti2015@gmail.com>
2021-09-01 12:30:55 +0530

    response routing done

--- DCA/src/app/app-routing.module.ts
+++ DCA/src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { CreateDeveloperComponent } from './Components/Developer/create-develope
 import { DeveloperDetailsComponent } from './Components/Developer/developer-details/developer-details/developer-details.component';
 import { DeveloperListComponent } from './Components/Developer/developer-list/developer-list/developer-list.component';
 import { UpdateDeveloperComponent } from './Components/Developer/update-developer/update-developer/update-developer.component';
+import { CreateResponseComponent } from './Components/Response/create-response/create-response/create-response.component';
+import { GetResponseComponent } from './Components/Response/get-response/get-response/get-response.component';
+import { UpdateResponseComponent } from './Components/Response/update-response/update-response/update-response.component';
 
 const routes: Routes = [
 
   {path: 'dev-list', component:DeveloperListComponent},
   {path: 'addNew', component:CreateDeveloperComponent},
   {path: 'devDetails', component:DeveloperDetailsComponent},
-  {path: 'update/:devid', component:UpdateDeveloperComponent}
+  {path: 'update/:devid', component:UpdateDeveloperComponent},
+  {path:'createResponse',component:CreateResponseComponent},
+  {path:'getResponses',component:GetResponseComponent},
+  {path:'updateResponse',component:UpdateResponseComponent}
 ];
 
 @NgModule({
