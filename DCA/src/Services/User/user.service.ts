5e73fd09df79818a55633441ede3ce2db2ebc4c2
shashudhar <shshidharshiva123@gmail.com>
2021-09-03 00:26:08 +0530

    user service added

--- DCA/src/Services/User/user.service.ts
+++ DCA/src/Services/User/user.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/Models/User/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  baseURL = 'http://localhost:8080/user';
+  constructor(private http:HttpClient) { }
+
+  createUser(user:User):Observable<any>{
+    return this.http.post(this.baseURL,user
+      
+      )
+  }
+
+  getUserById(userId:string):Observable<any>{
+    return this.http.get(`${this.baseURL}/${userId}`)
+
+  }
+
+  getAllUsers(): Observable<any> {
+    return this.http.get(this.baseURL)
+
+  }
+
+  Checklogin(userCredentials:User):Observable<any>{
+    return this.http.post(this.baseURL,userCredentials)
+  }
 }
