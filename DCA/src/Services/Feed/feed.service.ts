46ed0fbbe3a8460f2d72035a6a5531e10a419614
saipavan-k <saipavan.kalikota19@gmail.com>
2021-09-02 18:44:24 +0530

    Update feed.service.ts

--- DCA/src/Services/Feed/feed.service.ts
+++ DCA/src/Services/Feed/feed.service.ts
@@ -1,9 +1,56 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Feed } from 'src/Models/Feed/feed';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedService {
 
-  constructor() { }
+  baseURL = 'http://localhost:9090/feed';
+
+  constructor(private http: HttpClient) { }
+
+  getFeedsList(): Observable<any> {
+    return this.http.get(`${this.baseURL}/all`)
+    }
+  
+   addFeed(feed: Feed):Observable<any> {
+    return  this.http.post(this.baseURL, feed)
+  
+    }
+
+   getFeed(feedId: number): Observable<any> {
+    return this.http.get(`${this.baseURL}/${feedId}`)
+
+    }
+
+   deleteFeed(feedId: number): Observable<any> {
+      let url = `${this.baseURL}/${feedId}`
+      return this.http.delete(url, { responseType: 'text' })
+  
+    }
+
+   updateFeed(feed: Feed):Observable<any> {
+      return this.http.put(this.baseURL, feed)
+    }
+  
+   getFeedByDevId(devId: number): Observable<any> {
+      console.log(this.http.get(`${this.baseURL}/${devId}`))
+      return this.http.get(`${this.baseURL}/${devId}`);
+      
+    }
+  
+   getFeedByKeyword(keyword: string): Observable<any> {
+      console.log(this.http.get(`${this.baseURL}/${keyword}`));
+      return this.http.get(`${this.baseURL}/${keyword}`);
+    }
+
+   getFeedByTopic(topic: string): Observable<any> {
+      console.log(this.http.get(`${this.baseURL}/${topic}`));
+      return this.http.get(`${this.baseURL}/${topic}`);
+    }
+
 }
