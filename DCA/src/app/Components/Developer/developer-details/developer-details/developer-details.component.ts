d04c185c19cf77eeb459d797ed0a9f976e4f1326
Azmaths <47237313+Azmaths@users.noreply.github.com>
2021-09-08 12:45:49 +0530

    Update developer-details.component.ts

--- DCA/src/app/Components/Developer/developer-details/developer-details/developer-details.component.ts
+++ DCA/src/app/Components/Developer/developer-details/developer-details/developer-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Developer } from 'src/Models/Developer/developer';
 import { DeveloperService } from 'src/Services/Developer/developer.service';
+import { FeedService } from 'src/Services/Feed/feed.service';
 
 @Component({
   selector: 'app-developer-details',
@@ -12,10 +13,11 @@ export class DeveloperDetailsComponent implements OnInit {
 
   devId: number;
   developer: Developer;
+  developers: Developer[] = [];
 
 
   constructor(private route: ActivatedRoute,private router: Router,
-    private developerService: DeveloperService) { }
+    private developerService: DeveloperService, private feedService: FeedService) { }
 
   ngOnInit() {
     this.developer = new Developer();
@@ -32,6 +34,19 @@ export class DeveloperDetailsComponent implements OnInit {
   list(){
     this.router.navigate(['dev-list']);
   }
-  
+  delete(feedId: number): void {
 
+    this.feedService.deleteFeed(feedId).subscribe(
+      (response) => {
+        // this.message = response;
+        // this.loadData();
+        // console.log(this.message);
+      },
+      (errorResponse) => {
+        // this.message = errorResponse.error.errorMessage
+        // this.loadData();
+      }
+    )
+
+}
 }
