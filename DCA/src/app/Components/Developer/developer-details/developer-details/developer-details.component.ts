fcf077f521b0efcb3cc07bf145f2d835061af7e5
Azmaths <47237313+Azmaths@users.noreply.github.com>
2021-09-08 12:58:34 +0530

    Revert "Update developer-details.component.ts"
    This reverts commit d04c185c19cf77eeb459d797ed0a9f976e4f1326.

--- DCA/src/app/Components/Developer/developer-details/developer-details/developer-details.component.ts
+++ DCA/src/app/Components/Developer/developer-details/developer-details/developer-details.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Developer } from 'src/Models/Developer/developer';
 import { DeveloperService } from 'src/Services/Developer/developer.service';
-import { FeedService } from 'src/Services/Feed/feed.service';
 
 @Component({
   selector: 'app-developer-details',
@@ -13,11 +12,10 @@ export class DeveloperDetailsComponent implements OnInit {
 
   devId: number;
   developer: Developer;
-  developers: Developer[] = [];
 
 
   constructor(private route: ActivatedRoute,private router: Router,
-    private developerService: DeveloperService, private feedService: FeedService) { }
+    private developerService: DeveloperService) { }
 
   ngOnInit() {
     this.developer = new Developer();
@@ -34,19 +32,6 @@ export class DeveloperDetailsComponent implements OnInit {
   list(){
     this.router.navigate(['dev-list']);
   }
-  delete(feedId: number): void {
+  
 
-    this.feedService.deleteFeed(feedId).subscribe(
-      (response) => {
-        // this.message = response;
-        // this.loadData();
-        // console.log(this.message);
-      },
-      (errorResponse) => {
-        // this.message = errorResponse.error.errorMessage
-        // this.loadData();
-      }
-    )
-
-}
 }
